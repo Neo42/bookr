939012f065e79dcf5a3b3a8b2feddf380b8d144f
Jiang Hao <giovannilance94@gmail.com>
2021-05-14 14:02:23 +0800

    feat: :sparkles: wire up `App`, api client & backend to get user from local after login

--- src/app.js
+++ src/app.js
@@ -4,15 +4,26 @@ import * as React from 'react'
 import UnAuthorizedApp from './unauthorized-app'
 import AuthorizedApp from './authorized-app'
 import * as auth from './auth-provider'
+import client from './utils/api-client'
 
 export default function App() {
   const [user, setUser] = React.useState(null)
+  React.useEffect(() => getUser().then((u) => setUser(u)), [])
 
   const login = (form) => auth.login(form).then((u) => setUser(u))
   const register = (form) => auth.register(form).then((u) => setUser(u))
   const logout = () => {
     auth.logout()
     setUser(null)
+  }
+
+  async function getUser() {
+    let user = null
+    const token = await auth.getToken()
+    if (token) {
+      user = await client('me', {token})
+    }
+    return user
   }
 
   return user ? (
--- src/utils/api-client.js
+++ src/utils/api-client.js
@@ -1,5 +1,16 @@
-export default function client(endpoint, customConfig = {}) {
-  const config = {method: 'GET', ...customConfig}
+export default function client(
+  endpoint,
+  {token, headers: customHeaders, ...customConfig} = {}
+) {
+  const config = {
+    method: 'GET',
+    headers: {
+      // "Bearer", not "Bear"
+      Authorization: token ? `Bearer ${token}` : undefined,
+      ...customHeaders,
+    },
+    ...customConfig,
+  }
   return window
     .fetch(`${process.env.REACT_APP_API_URL}/${endpoint}`, config)
     .then(async (response) => {
