2f7f37a9ad4add9a884e2b704081c42f4eb8856a
Jiang Hao <giovannilance94@gmail.com>
2021-05-14 16:03:07 +0800

    feat(utils): :sparkles: add post request support for api client

--- src/utils/api-client.js
+++ src/utils/api-client.js
@@ -1,19 +1,27 @@
+import * as auth from '../auth-provider'
+
 export default function client(
   endpoint,
-  {token, headers: customHeaders, ...customConfig} = {}
+  {data, token, headers: customHeaders, ...customConfig} = {}
 ) {
   const config = {
-    method: 'GET',
+    method: data ? 'POST' : 'GET',
     headers: {
-      // "Bearer", not "Bear"
-      Authorization: token ? `Bearer ${token}` : undefined,
+      Authorization: token ? `Bearer ${token}` : undefined, // "Bearer", not "Bear"
+      'Content-Type': data ? 'application/json' : undefined,
       ...customHeaders,
     },
     ...customConfig,
   }
+
   return window
     .fetch(`${process.env.REACT_APP_API_URL}/${endpoint}`, config)
     .then(async (response) => {
+      if (response.status === 401) {
+        await auth.logout()
+        window.location.assign(window.location)
+        return Promise.reject({message: '请重新进行用户验证。'})
+      }
       const data = await response.json()
       if (response.ok) {
         return data
