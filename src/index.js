393b3464db089361b3646bba4648e85f1457bb19
Jiang Hao <giovannilance94@gmail.com>
2021-05-24 16:54:09 +0800

    refactor(auth): ♻️ "co-locate" auth logic with `AppProvider` component

--- src/index.js
+++ src/index.js
@@ -1,27 +1,12 @@
-import 'styles/bootstrap'
 import * as React from 'react'
 import ReactDOM from 'react-dom'
-import {ReactQueryConfigProvider} from 'react-query'
 import App from 'apps/app'
-import {AuthProvider} from 'auth/context'
-
-const queryConfig = {
-  queries: {
-    useErrorBoundary: true,
-    refetchOnWindowFocus: false,
-    retry(failureCount, error) {
-      if (error.status === 404) return false
-      else if (failureCount < 2) return true
-      else return false
-    },
-  },
-}
+import AppProvider from 'auth'
+import 'styles/bootstrap'
 
 ReactDOM.render(
-  <ReactQueryConfigProvider config={queryConfig}>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </ReactQueryConfigProvider>,
+  <AppProvider>
+    <App />
+  </AppProvider>,
   document.getElementById('root'),
 )
