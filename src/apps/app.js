86d76c58f2970992684f5b9a6f7da9ffa5cb1f6d
Jiang Hao <giovannilance94@gmail.com>
2021-05-25 16:56:32 +0800

    perf(apps): ⚡️ enable webpack prefetch for dynamic import

--- src/apps/app.js
+++ src/apps/app.js
@@ -2,7 +2,14 @@ import * as React from 'react'
 import {useAuth} from 'auth/context'
 import {FullPageSpinner} from 'components/lib'
 
-const AuthenticatedApp = React.lazy(() => import('./authenticated-app'))
+const AuthenticatedApp = React.lazy(() =>
+  // AuthenticatedApp will always be needed after a user login
+  // so here we tell webpack to prefetch it for us
+  import(
+    /* webpackPrefetch: true */
+    './authenticated-app'
+  ),
+)
 const UnAuthenticatedApp = React.lazy(() => import('./authenticated-app'))
 
 export default function App() {
