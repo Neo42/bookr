cd9da043779b270ec757c8e60f9b1cdd7811ba57
Jiang Hao <giovannilance94@gmail.com>
2021-05-28 17:50:46 +0800

    refactor(components): 🎨 change react import style

---
+++ src/screens/__tests__/book.js
@@ -0,0 +1,5 @@
+import * as React from 'react'
+import {render, screen, waitFor} from '@testing-library/react'
+import {queryCache} from 'react-query/types'
+
+test.todo('render all the book information')
